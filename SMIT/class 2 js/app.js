d2d051121411c38f2e4f5a2c992520e3cf06036d
class12 <mansoorihassan409@gmail.com>
2025-12-04 14:33:35 +0500

    toss app completed

--- SMIT/class 2 js/app.js
+++
@@ -1,3 +0,0 @@
-console.log("hassan");
-console.log("Sharif");
-console.log("17");
--- SMIT/freelancing-landing-page/src/app.js
+++
@@ -1,7 +0,0 @@
-// let links = document.getElementById("nav-links");
-// let humburger = document.getElementById("humburger");
-//     humburger.addEventListener("click" , () => {
-//         links.classList.toggle("active");
-//     });
-
-// document.body.style.scrollBehavior = "smooth";
